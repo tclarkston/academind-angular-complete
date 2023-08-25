7b422d0b77436bf37c8555a3bf37895896115e5f
Todd Clarkston <todd@wireservers.com>
2023-08-25 10:51:27 -0600

    Working dropdown.

--- Course Project Final/src/app/shared/dropdown.directive.ts
+++ Course Project Final/src/app/shared/dropdown.directive.ts
@@ -1,14 +1,14 @@
-import { Directive, HostListener, HostBinding, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  @HostBinding('') isOpen = false;
+  @HostBinding('class.open') isOpen = false;
 
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
   }
  
-  constructor(private tempRef: TemplateRef<any>, private viewRef: ViewContainerRef) { }
+  constructor() { }
 }
