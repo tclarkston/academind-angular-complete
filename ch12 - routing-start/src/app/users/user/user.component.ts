d69d589c40a8b8607da1af996d2d075a315d3c3b
Todd Clarkston <todd@wireservers.com>
2023-08-28 08:20:12 -0600

    Updated route params.

--- ch12 - routing-start/src/app/users/user/user.component.ts
+++ ch12 - routing-start/src/app/users/user/user.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,7 +12,15 @@ export class UserComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.user = { id: this.route.snapshot.params['id'], 
-    name: this.route.snapshot.params['name'] }
+    this.user = { 
+      id: this.route.snapshot.params['id'], 
+      name: this.route.snapshot.params['name'] 
+    }
+
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+    });
   }
 }
