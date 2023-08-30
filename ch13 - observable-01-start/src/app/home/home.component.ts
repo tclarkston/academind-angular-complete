a9065f90bc0d1f05dbed69797e1f63dc92789719
Todd Clarkston <todd@wireservers.com>
2023-08-29 21:32:44 -0600

    Updated observable

--- ch13 - observable-01-start/src/app/home/home.component.ts
+++ ch13 - observable-01-start/src/app/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private intSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    this.intSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    });
   }
 
+  ngOnDestroy(): void {
+      this.intSubscription.unsubscribe();
+  }
 }
